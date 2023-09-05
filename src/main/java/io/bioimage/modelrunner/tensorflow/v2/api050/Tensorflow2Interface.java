57b5c82ae3fbf2d9fe4beda3d9ade9be613430dd
carlosuc3m <100329787@alumnos.uc3m.es>
2023-09-05 19:45:51 +0200

    updat to new jdll

--- src/main/java/io/bioimage/modelrunner/tensorflow/v2/api050/Tensorflow2Interface.java
+++ src/main/java/io/bioimage/modelrunner/tensorflow/v2/api050/Tensorflow2Interface.java
@@ -184,7 +184,7 @@ public class Tensorflow2Interface implements DeepLearningEngineInterface {
     public Tensorflow2Interface() throws IOException
     {
     	boolean isWin = PlatformDetection.isWindows();
-    	boolean isIntel = new PlatformDetection().getArch().equals(PlatformDetection.ARCH_X86_64);
+    	boolean isIntel = PlatformDetection.getArch().equals(PlatformDetection.ARCH_X86_64);
     	if (true || (isWin && isIntel)) {
     		interprocessing = true;
     		tmpDir = getTemporaryDir();
@@ -205,7 +205,7 @@ public class Tensorflow2Interface implements DeepLearningEngineInterface {
     		interprocessing = false;
     	} else {
     		boolean isMac = PlatformDetection.isMacOS();
-        	boolean isIntel = new PlatformDetection().getArch().equals(PlatformDetection.ARCH_X86_64);
+        	boolean isIntel = PlatformDetection.getArch().equals(PlatformDetection.ARCH_X86_64);
         	if (isMac && isIntel) {
         		interprocessing = true;
         		tmpDir = getTemporaryDir();        		
